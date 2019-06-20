0d63c94947bef6e5f9262d018e1dfb5bf9ce7a71
LDN <ledenevdn@gmail.com>
2019-06-20 16:48:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Sinatra Bootstrap v.4.1.3
+=======================
+
+Updated version of https://github.com/bootstrap-ruby/sinatra-bootstrap
---
+++ app.rb
@@ -0,0 +1,8 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! Original pattern has been modified for me =)"			
+end
